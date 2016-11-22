3aebf6647821b803b8bdeb23b40ea6648e94b4b1
gujunmin <3013366498@qq.com>
2016-11-22 20:11:32 +0800

    loginModal 完成

---
+++ modules/loginModal/README.md
@@ -0,0 +1,12 @@
+# loginModal 组件
+
+## 简述
+
+此组件用于弹出一个 modal 用于登录输入。
+
+使用到的技术有：
+
+1. html/css(oocss)
+2. normalize
+3. jquery
+4. fontawesome
--- modules/loginModal/js/index.js
+++ modules/loginModal/js/index.js
@@ -1,5 +1,62 @@
-$(function() {
-    alert('ok');
-});
+/*eslint no-console: ["error", { allow: ["log", "warn", "error"] }] */
 
-// TODO: eslintrc 文件 & 写交互代码
+$(function() {
+    /**
+     * 获取元素
+     */
+    var showModalLogin = $('#js_show_modal_login');
+    var modalLogin = $('#js_modal_login');
+    var modalLoginClose = $('#js_modal_login_close');
+    var modalLoginUsername = $('#js_modal_login_username');
+    var modalLoginPassword = $('#js_modal_login_password');
+    var modalLoginSubmit = $('#js_modal_login_submit');
+
+    /**
+     * 表单验证
+     */
+    var validateLoginForm = function() {
+        var _flag = true;
+
+        if (modalLoginUsername.val().length <= 0) {
+            _flag = false;
+            modalLoginUsername.addClass('error');
+        } else if (modalLoginPassword.val().length <= 0) {
+            _flag = false;
+            modalLoginPassword.addClass('error');
+        }
+
+        return _flag;
+    };
+
+    modalLoginUsername.on('input', function() {
+        modalLoginUsername.removeClass('error');
+    });
+
+    modalLoginPassword.on('input', function() {
+        modalLoginPassword.removeClass('error');
+    });
+
+    /**
+     * 登录模态框的显示和关闭
+     */
+    showModalLogin.on('click', function() {
+        modalLogin.removeClass('f-dn');
+    });
+
+    modalLoginClose.on('click', function() {
+        modalLogin.addClass('f-dn');
+    });
+
+    /**
+     * 表单提交
+     */
+    modalLoginSubmit.on('click', function(event) {
+        event.preventDefault();
+
+        if (validateLoginForm()) {
+            modalLoginClose.triggerHandler('click');
+
+            console.log('登录成功！！！');
+        }
+    });
+});
